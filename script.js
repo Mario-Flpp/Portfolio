18efdfdb30c04f52f79d7acaeee0327e80363915
Mad-Scripts <114199273+Lemonthzgg@users.noreply.github.com>
2024-08-18 07:35:04 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Cybersecurity Analist", "Red Team", "Python Senior"],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true
+
+})
